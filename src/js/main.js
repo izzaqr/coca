cd297107a7d8105c579d77a354e10001b640e175
izzaqr <amirkazanfarov@gmail.com>
2024-05-22 23:53:12 +0300

    Created hero section and fidex header

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,6 @@ import '../assets/scss/main.scss';
 // components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
-import { stickyHeader } from './components/stickyheader.js';
 
 useTheme();
 useBurger();
-stickyHeader();
