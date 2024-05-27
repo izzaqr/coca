181a06ec3c733dc8da9563a62f32296a5d19afb8
izzaqr <amirkazanfarov@gmail.com>
2024-05-27 17:43:09 +0300

    Created sticky header

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import '../assets/scss/main.scss';
 // components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
+import { stickyHeader } from './components/stickyheader.js';
 
 useTheme();
 useBurger();
+stickyHeader();
