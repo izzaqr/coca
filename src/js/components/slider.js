6ef37316e63a71c08d8f6c7c7a34f6a181340c21
izzaqr <amirkazanfarov@gmail.com>
2024-05-31 11:23:34 +0300

    Created slider butons

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,13 +1,18 @@
 import Swiper from 'swiper';
-import { Autoplay } from 'swiper/modules';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 
 export const useInsightSlider = () => {
   new Swiper('.insight__slider', {
+    modules: [Navigation, Pagination],
     slidesPerView: 'auto',
     spaceBetween: 32,
     loop: true,
     centeredSlides: true,
+    navigation: {
+      nextEl: '.insight__slider-btns-right',
+      prevEl: '.insight__slider-btns-left',
+    },
     breakpoints: {
       993: {
         centeredSlides: false,
