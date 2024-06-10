eb9f26d960b80d7079ae5d5bde5795a25b46ae22
izzaqr <amirkazanfarov@gmail.com>
2024-06-10 23:37:58 +0300

    Created about:team section

--- src/js/about.js
+++ src/js/about.js
@@ -4,9 +4,10 @@ import '/scss/about.scss';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { useStickyHeader } from './components/useStickyHeader.js';
-import { useAboutSlider } from './components/about/slider.js';
+import { useAboutSlider, useTeamSlider } from './components/about/slider.js';
 
 useTheme();
 useBurger();
 useStickyHeader();
 useAboutSlider();
+useTeamSlider();
